efda6f5da96407c1d87a492caff2e07cc133824a
Djurre <djurreaikema@gmail.com>
2024-02-12 15:44:12 +0100

    WPF Tutorial #6

---
+++ WpfApp1/View/UserControls/MenuBar.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace WpfApp1.View.UserControls;
+
+public partial class MenuBar : UserControl
+{
+    public MenuBar()
+    {
+        InitializeComponent();
+    }
+}
--- WpfApp1/obj/Debug/net8.0-windows/WpfApp1.AssemblyInfo.cs
+++ WpfApp1/obj/Debug/net8.0-windows/WpfApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WpfApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7b0f51c1586d1afac6e874dfdf61ade286cdf0a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ba3bf327ceaa15a60cb01ea4bf0ade411dcb6a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("WpfApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WpfApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
