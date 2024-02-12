2428709db561296865fa2c67d41f5422443524e7
Djurre <djurreaikema@gmail.com>
2024-02-12 14:01:02 +0100

    WPF Tutorial #4

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -4,8 +4,28 @@ namespace WpfApp1;
 
 public partial class MainWindow : Window
 {
+    private bool running = false;
+
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void BtnToggleRun_OnClick(object sender, RoutedEventArgs e)
+    {
+        if (running)
+        {
+            // stop
+            TbStatus.Text = "Stopped";
+            BtnToggleRun.Content = "Run";
+        }
+        else
+        {
+            // run
+            TbStatus.Text = "Running";
+            BtnToggleRun.Content = "Stop";
+        }
+
+        running = !running;
     }
 }
