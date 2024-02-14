e540d70e7036dd6a1d2689b195cb26957a4281bf
Djurre <djurreaikema@gmail.com>
2024-02-14 11:58:55 +0100

    WPF Tutorial #12

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,5 +1,5 @@
-﻿using System.Windows;
-using WinForms = System.Windows.Forms;
+﻿using System.Collections;
+using System.Windows;
 
 namespace WpfApp1;
 
@@ -8,17 +8,39 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        lvEntries.Items.Add("a");
+        lvEntries.Items.Add("b");
+        lvEntries.Items.Add("c");
     }
 
-    private void BtnFire_OnClick(object sender, RoutedEventArgs e)
+    private void BtnAdd_OnClick(object sender, RoutedEventArgs e)
     {
-        WinForms.FolderBrowserDialog dialog = new FolderBrowserDialog();
-        dialog.InitialDirectory = "C:\\Programming\\Projects\\wpf-practice";
-        WinForms.DialogResult result = dialog.ShowDialog();
+        lvEntries.Items.Add(txtEntry.Text);
+        txtEntry.Clear();
+    }
 
-        if (result == WinForms.DialogResult.OK)
+    private void BtnDelete_OnClick(object sender, RoutedEventArgs e)
+    {
+        // int index = lvEntries.SelectedIndex;
+        // lvEntries.Items.RemoveAt(index);
+
+        // object item = lvEntries.SelectedItem;
+        // MessageBoxResult result = MessageBox.Show($"Are you sure you want to delete: {item}?", "Sure?", MessageBoxButton.YesNo);
+        // if (result == MessageBoxResult.Yes)
+        //     lvEntries.Items.Remove(item);
+
+        var items = lvEntries.SelectedItems;
+        MessageBoxResult result = MessageBox.Show($"Are you sure you want to delete: {items.Count} items?", "Sure?", MessageBoxButton.YesNo);
+        if (result == MessageBoxResult.Yes)
         {
-            string folder = dialog.SelectedPath;
+            var itemsList = new ArrayList(items);
+            foreach (var item in itemsList)
+                lvEntries.Items.Remove(item);
         }
+    }
+
+    private void BtnClear_OnClick(object sender, RoutedEventArgs e)
+    {
+        lvEntries.Items.Clear();
     }
 }
